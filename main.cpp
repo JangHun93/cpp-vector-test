f7e3d816951a0acab3cf99e7563f7c45d7fd77a1
JangHun93 <pain3992@gmail.com>
2024-05-24 10:07:03 +0900

    벡터 제거

--- main.cpp
+++ main.cpp
@@ -7,35 +7,53 @@ using namespace std;
 // 1 ~ 10까지 숫자를 동적으로 할당받아 배열에 넣고
 // 입력받은 숫자를 임의 위치에 추가해서 배열 크기를 늘리고
 // 출력하는 프로그램을 만들어 보세요.
-
-void SetArrayRandomlyByInput(vector<int> numList)
+void SetArrayRandomlyByInput(int *numList)
 {
     // 임의의 위치
-    int randomIndex = rand() % numList.size();
+    int randomIndex = rand() % 10;
+
     // 입력 값
     int inputNumber = 0;
     cout << "숫자을 입력하세요: ";
     cin >> inputNumber;
 
-    numList.insert(numList.begin() + randomIndex, inputNumber);
-
-    for (auto literator = numList.begin(); literator < numList.end(); ++literator)
+    int *newList = new int;
+    for (int i = 0; i <= randomIndex; i++)
     {
-        cout << *literator << endl;
+        if (i == randomIndex)
+        {
+            // 랜덤 인덱스
+            int temp = newList[randomIndex];
+            newList[randomIndex] = inputNumber;
+            newList[randomIndex + 1] = temp;
+        }
+        else
+        {
+            newList[i] = numList[i];
+        }
     }
-    //
-    // cout << name << endl;
+
+    for (int i = randomIndex; i < 11; i++)
+    {
+        newList[i + 1] = numList[i];
+    }
+
+    for (int i = 0; i < 11; i++)
+    {
+        cout << newList[i] << ", ";
+    }
+    cout << endl;
 }
 
 int main()
 {
-
     srand(time(nullptr));
-    vector<int> numList;
+
+    int *numList = new int;
 
     for (int i = 1; i <= 10; i++)
     {
-        numList.push_back(i);
+        numList[i - 1] = i;
     }
 
     SetArrayRandomlyByInput(numList);
